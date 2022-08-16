ee8fce3962dc3a1c533bf94262602c4e786c70e0
David <davidongaroh@gmail.com>
2022-08-16 14:09:31 +0300

    examples

--- main.rb
+++ main.rb
@@ -2,46 +2,71 @@
 
 # 1.0 Printing Methods
 # TODO: puts
+puts 'Hello Sexy'
+puts 'Wagwan Brejin!'
 
 # TODO: print
-
+print 'Another Hello sexy'
+print ' aye'
 # TODO: pp
-
 # 2.0 Strings
 # TODO: CREATE A STRING
-
-# TODO: CREATE A STRING WITH SINGLE QUOTES
-
+name ="Ras"
+# TODO: CREATE A STRING WITH DOUBLE QUOTES
+girl = "Shawty"
 # TODO: INTERPOLATE STRINGS
-
+text_msg = "Hello #{girl}, #{name} wants to smash "
+puts text_msg
 # TODO: String methods (upcase, downcase, capitalize)
+puts text_msg.upcase
+puts text_msg.capitalize
+
 
 # 3.0 Numbers
 # TODO: Integers
-
+weight = 67
 # TODO: Float
-
+puts 20/2.5
 # TODO: Number methods (floor, ceil, to_i, to_f)
+puts '12.6'.to_i
+puts 39.7.floor
+puts 0.001.ceil
+puts 0.9.to_i
+puts 50/3.to_f
+puts 50/3
+puts 2%5 
 
 
 # 4.0 Boolean
-# TODO: True / Truthy
-
-# TODO: False / Falsy
-
+# TODO: True / Truthy(anything that has avalue except the value false)
+puts is_hot= true
+# TODO: False / Falsy(nil,false)
+puts is_rich = false
 # 5.0 Symbols
 # TODO: Create symbol
-
+puts :Moringa
+tree = :tingizaMti, :KFS
+pp tree
 
 # 6.0 Arrays
 # TODO: Create array natively
+memes = ['hello', 'choir', "bomas"]
+others = 'chebubae', 'baba'
+pp others
 
 # TODO: Create array using Array class
-
+roads = Array.new
+roads.push('Great North Road','Trans Africa Highway')
+pp roads
 # 7.0 Hashes
 # TODO: Create Hash
+hunnies = {name:"Ruth", age:22, height:164, team:"blue", area:"Kile"}
+puts hunnies
+pp hunnies
+pp hunnies[:name]
 
 # TODO: Create Hash using Hash class
+student = Hash.new
 
 # 8.0 Methods
 # TODO: Method with enclosed params
